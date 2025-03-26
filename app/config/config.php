4d04a87cdbd33735f4fba00707d846de7b92e2ef
Sarabjeet Singh <Saraabjeet.singh40@gmail.com>
2025-03-26 15:04:20 -0400

    fix app

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,6 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'sarab');
-define('DB_PASS', '1234<?php
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>');
+define('DB_PASS', '1234');
 define('DB_NAME', 'my_database');
 
